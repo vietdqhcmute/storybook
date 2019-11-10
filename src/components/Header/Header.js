85f7e28071aaef5e05a6a08a2cf469faeabc6f2b
Viet Do <vietdqhcmute@gmail.com>
2019-11-10 15:48:41 +0700

    Initial

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Header extends Component {
+  state = {  }
+  render() {
+    return (
+    <div>
+      <h2>This is just a header</h2>
+    </div>
+  );
+  }
+}
+
+export default Header;
---
+++ src/scenes/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class HomePage extends Component {
+  state = {  }
+  render() {
+    return (
+      <div>
+        <h2>This is content of homepage</h2>
+      </div>
+     );
+  }
+}
+
+export default HomePage;
