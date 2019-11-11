0b974f78cc8d091efcebf7f7f596eee8381d7f2a
Viet Do <vietdqhcmute@gmail.com>
2019-11-11 15:45:07 +0700

    Config carousel and header

--- src/scenes/HomePage/HomePage.js
+++ src/scenes/HomePage/HomePage.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import './HomePage.scss';
+
+import Carousel from '../../components/Carousel/Carousel';
+
 class HomePage extends Component {
-  state = {  }
+  state = {
+    carousels:[
+      {title:"Frozen 2: Queen of Frozen",image_url:"https://phephim.vn:3004/static/1570521435954-zgdkbbmkq_image.jpeg"},
+      {title:"Farewell",image_url:"https://phephim.vn:3004/static/1570592833941-2vt9awe66_image.jpeg"},
+      {title:"Zombieland",image_url:"https://phephim.vn:3004/static/1571884778466-u5bfc6dyb_image.jpeg"},
+      {title:"Joker: Prince of crime",image_url:"https://phephim.vn:3004/static/1567655636252-sq7k8jcsx_image.jpeg"}
+    ]
+  }
   render() {
     return (
       <div className="homepage">
         <div className="container">
-          <h2>This is content of homepage</h2>
+          <h3>Top trending movies</h3>
+          <div className="carousel_container">
+            {this.state.carousels.map((element, i) => <Carousel image_url={element.image_url} title={element.title}/>)}
+          </div>
         </div>
       </div>
     );
